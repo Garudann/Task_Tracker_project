41c23e7cc80759728543c8eb02255d2ba3024710
Garudann <gk1256177@gmail.com>
2025-01-27 14:45:16 +0530

    dashboard integerated with js

--- login.js
+++ login.js
@@ -8,7 +8,7 @@ document.getElementById('login_form').addEventListener('submit', function(event)
       document.getElementById('loginStatus').textContent = 'Login successful!';
       document.getElementById('loginStatus').style.color = 'green';
       //if logged int succesfully, it will redirect to this page.
-      window.location.href="D:/Personal/Create%20your%20own%20Image/project/dashboard.html"
+      window.location.href='dashboard.html'
   } else {
       document.getElementById('loginStatus').textContent = 'Invalid username or password.';
       document.getElementById('loginStatus').style.color = 'red';
